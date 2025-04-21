2fe62711199a331a9db7e949a8c4d3f69655469d
Faimo-M <faimom258@gmail.com>
2025-04-21 23:08:21 +0200

    criacao dos diretorios modelo

---
+++ Modelos/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficha3.Modelos
+{
+    public class Categoria
+    {
+        public string Nome { get; set; }
+
+        public Categoria(string nome)
+        {
+            Nome = nome;
+        }
+
+        
+    }
+}
