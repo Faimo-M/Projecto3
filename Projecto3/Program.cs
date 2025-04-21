ebeb734dc570963666456c6a10607f87bad5b9fb
Faimo-M <faimom258@gmail.com>
2025-04-21 23:19:40 +0200

    atualizacao do main

--- Projecto3/Program.cs
+++ Projecto3/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Projecto3
+﻿using Ficha3.Interfaces;
+using Ficha3.Modelos;
+using Ficha3.Servicos;
+using Ficha3.Utilitarios;
+
+namespace Projecto3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, tester!");
+            // criar as categorias dos produtos
+            Categoria roupas = new Categoria("Roupas");
+            Categoria eletronicos = new Categoria("Eletronicos");
+
+            //criar produtos
+            Produto produto = new Produto("cac", roupas, 255);
+            Produto produto1 = new Produto("laptop", eletronicos, 1500);
+
+            //adicionar produtos ao estoque
+
+            IEstoqueServices estoque = new EstoqueServices();
+            if (Validador.Validar(produto))//vai verificar se o produto eh valido
+            {
+                estoque.AdicionarProduto(produto);
+                estoque.AdicionarProduto(produto1);
+            }
+            else
+            {
+                Console.WriteLine($"Produto invalido: {produto.Nome}");
+            }
+
+            // listar todos os produtos
+
+            List<Produto> todosProdutos = estoque.ListarProdutos();
+            Console.WriteLine(" a listar todos Produtos ");
+            foreach (Produto p in todosProdutos)
+            {
+                Console.WriteLine($"Nome: {p.Nome} \npreco: {p.Preco} \nCategoria: {p.Categoria.Nome}");
+            }
+
+            //pesquiar por categoria
+            Console.WriteLine("prodtudos da categoria roupas");
+            List<Produto> roupasEncontradas = estoque.BuscarPorCategoria("roupas");
+            foreach (Produto p in roupasEncontradas)
+            {
+                Console.WriteLine($"Nome: {p.Nome} \npreco: {p.Preco}");
+
+            }
         }
     }
 }
