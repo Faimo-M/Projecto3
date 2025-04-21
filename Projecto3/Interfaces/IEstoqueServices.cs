b2c879e6ff07187dcdfe987daddec8bf675c8987
Faimo-M <faimom258@gmail.com>
2025-04-21 23:01:20 +0200

    criacao e adicao da interface

---
+++ Projecto3/Interfaces/IEstoqueServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ficha3.Modelos;
+
+namespace Ficha3.Interfaces
+{
+    public interface IEstoqueServices
+    {
+        public void AdicionarProduto( Produto produto);// este metodo diz que vai receber um objecto do tipo produto
+   
+         List<Produto> ListarProdutos();// vai retornar uma lista com os produtos
+        List<Produto> BuscarPorCategoria( string nomeCategoria); // vai retornar uma lista com produtos daquela categoria
+    }
+}
