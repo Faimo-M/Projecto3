82059a66925cd083eba71e0f52d58cc8c9efa4b1
Faimo-M <faimom258@gmail.com>
2025-04-21 23:12:36 +0200

    foi adionada a pasta modelos e suas classes

---
+++ Projecto3/Modelos/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficha3.Modelos
+{
+    public class Produto
+    {
+        public string Nome {  get; set; }
+        public Categoria Categoria { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, Categoria categoria, double preco)
+        {
+            Nome = nome;
+            Categoria = categoria;
+            Preco = preco;
+        }
+    }
+}
