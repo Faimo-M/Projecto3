ff928fa325283ba744f7ec47e272532983209303
Faimo-M <faimom258@gmail.com>
2025-04-21 23:15:01 +0200

    adicionda a pasta utilitario.

---
+++ Projecto3/Utilitarios/Validador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ficha3.Modelos;
+
+namespace Ficha3.Utilitarios
+{
+    public class Validador
+    {
+        public static bool Validar(Produto produto)
+        {
+            if (produto.Nome.Length < 3)
+            {
+                return false;
+            }
+            return true;
+
+
+        }
+    }
+}
